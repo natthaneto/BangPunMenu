9a724232aa6192113a3332fe45fa2bdb660786bf
PedGrob <micxerlas123@gmail.com>
2026-03-09 17:20:35 +0700

    build docs

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,10 @@ export default defineConfig({
     react(),
     legacy()
   ],
+  base: './', // ใช้ Relative Path เพื่อให้เรียกไฟล์จากโฟลเดอร์ไหนก็ได้
+  build: {
+    outDir: 'docs', // เปลี่ยนจาก dist เป็น docs
+  },
   test: {
     globals: true,
     environment: 'jsdom',
